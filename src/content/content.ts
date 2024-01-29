53e1788b1954c3957d3eff5d615f626d9e9c12a4
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-29 18:13:45 +0100

    Generate balloons at random intervals close #6

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,9 +1,19 @@
 import Balloon, { balloonContainer } from "../balloon";
+import { generateRandomNumber } from "../utils";
 import "./style.css";
 
 document.body.appendChild(balloonContainer);
 
-const balloonInterval = setInterval(() => {
+const generateRandomInterval = () => generateRandomNumber(3000, 6000);
+
+let balloonInterval = setTimeout(function createAndRiseBalloon() {
   const balloon = new Balloon();
   balloon.rise();
-}, 6000);
+
+  // Set the next interval
+  clearInterval(balloonInterval);
+  const a = generateRandomInterval();
+  balloonInterval = setTimeout(createAndRiseBalloon, a);
+  console.log("balloonInterval", a);
+  
+}, generateRandomInterval());
