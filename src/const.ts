dfe2cbda08e7ea0c1266cf897e2867e6a481cb8a
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-29 17:02:13 +0100

    Move icon location

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const balloonImageUrl = chrome.runtime.getURL("icons/icon.png");
+export const balloonImageUrl = chrome.runtime.getURL("resources/images/icon.png");
 
 type UpdateCounterMessage = {
   action: "updateCounter";
