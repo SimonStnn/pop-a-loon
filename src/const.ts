e47fa1e7e56c8eeec17f310d01122a5af315b412
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-29 17:16:53 +0100

    Add pop sound #5

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,5 @@
 export const balloonImageUrl = chrome.runtime.getURL("resources/images/icon.png");
+export const popSoundUrl = chrome.runtime.getURL("resources/audio/pop.mp3");
 
 type UpdateCounterMessage = {
   action: "updateCounter";
