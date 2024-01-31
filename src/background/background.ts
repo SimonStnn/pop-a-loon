2289bc5e66465625af4032a04c13f9bcf90ab5de
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-31 12:40:26 +0100

    Add background service worker and update badge counter handling close #7

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,22 @@
+import { Message } from "../const";
+import { storage } from "../utils";
 
+(async () => {
+  chrome.action.setBadgeText({
+    text: (await storage.get("balloonCount")).balloonCount.toString(),
+  });
+})();
+
+chrome.runtime.onMessage.addListener(function (
+  message: Message,
+  sender,
+  sendResponse
+) {
+  if (message.action === "updateCounter") {
+    console.log("Background Received updated counter:", message);
+
+    chrome.action.setBadgeText({
+      text: message.balloonCount.toString(),
+    });
+  }
+});
