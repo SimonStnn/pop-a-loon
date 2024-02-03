f9206864a6c543b95c29ca022efbf1ad96349344
SimonStnn <simon.stijnen.23@gmail.com>
2024-02-03 20:07:44 +0100

    Refactor message listener to arrow function

--- src/background/background.ts
+++ src/background/background.ts
@@ -9,16 +9,14 @@ import { storage } from "../utils";
   });
 })();
 
-chrome.runtime.onMessage.addListener(function (
-  message: Message,
-  sender,
-  sendResponse
-) {
-  if (message.action === "updateCounter") {
-    console.log("Background Received updated counter:", message);
+chrome.runtime.onMessage.addListener(
+  (message: Message, sender, sendResponse) => {
+    if (message.action === "updateCounter") {
+      console.log("Background Received updated counter:", message);
 
-    chrome.action.setBadgeText({
-      text: message.balloonCount.toString(),
-    });
+      chrome.action.setBadgeText({
+        text: message.balloonCount.toString(),
+      });
+    }
   }
-});
+);
