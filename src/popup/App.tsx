58622aa0f777720e99ae0f1c069b5672edd82f66
SimonStnn <simon.stijnen.23@gmail.com>
2024-02-03 20:15:01 +0100

    Update counter when receiving updateCounter message

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { storage } from "../utils";
+import { Message } from "../const";
 
 const App: React.FC = () => {
   const [balloonCount, setBalloonCount] = useState(0);
-  
+
   const fetchBalloonCount = async () => {
     const count = (await storage.get("balloonCount")).balloonCount;
     setBalloonCount(count || 0);
   };
+
+  chrome.runtime.onMessage.addListener(
+    (message: Message, sender, sendResponse) => {
+      if (message.action === "updateCounter") {
+        setBalloonCount(message.balloonCount);
+      }
+    }
+  );
 
   useEffect(() => {
     fetchBalloonCount();
