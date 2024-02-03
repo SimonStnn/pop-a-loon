79e35ce139be538a6a2e611b7495207a77183a03
SimonStnn <simon.stijnen.23@gmail.com>
2024-02-03 20:04:44 +0100

    Update webpack.config.js to include .tsx file extension

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,25 @@ module.exports = {
   entry: {
     background: "./src/background/background.ts",
     content: "./src/content/content.ts",
-    popup: "./src/popup/popup.ts",
+    popup: "./src/popup/index.tsx",
   },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: [".tsx",".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
