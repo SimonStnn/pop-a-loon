958eea7f4d68e785df61800eeb9dd2d05401c9f5
SimonStnn <simon.stijnen.23@gmail.com>
2024-02-03 21:58:21 +0100

    Add routes

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Settings from "./Settings";
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
 
 // Import stylesheet
-import "./style.css"
+import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/settings" element={<Settings />} />
+    </Routes>
+  </Router>
+);
