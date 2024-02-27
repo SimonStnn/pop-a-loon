f7443dc469ff90af37f38d23e99750cdf37de03c
SimonStnn <simon.stijnen.23@gmail.com>
2024-02-27 19:15:43 +0100

    Add visibility protection to balloon class close #41

--- src/balloon.ts
+++ src/balloon.ts
@@ -1,25 +1,25 @@
-import { generateRandomNumber, sendMessage, storage } from "./utils";
-import { balloonImageUrl, popSoundUrl } from "./const";
+import { generateRandomNumber, sendMessage, storage } from './utils';
+import { balloonImageUrl, popSoundUrl } from './const';
 
-export const balloonContainer = document.createElement("div");
-balloonContainer.id = "balloon-container";
+export const balloonContainer = document.createElement('div');
+balloonContainer.id = 'balloon-container';
 
 export default class Balloon {
-  element: HTMLDivElement = document.createElement("div");
-  currentTopOffset: number = 100;
-  duration: number;
-  popSound: HTMLAudioElement = new Audio(popSoundUrl);
+  private element: HTMLDivElement = document.createElement('div');
+  private currentTopOffset: number = 100;
+  private duration: number;
+  private popSound: HTMLAudioElement = new Audio(popSoundUrl);
 
   constructor() {
     // Set the balloon's width and height to value between 50 and 200
-    this.element.classList.add("balloon");
+    this.element.classList.add('balloon');
     const size = generateRandomNumber(50, 75);
-    this.element.style.width = size + "px";
-    this.element.style.height = size + "px";
-    this.element.style.left = `calc(${generateRandomNumber(5, 95) + "vw"} - ${
+    this.element.style.width = size + 'px';
+    this.element.style.height = size + 'px';
+    this.element.style.left = `calc(${generateRandomNumber(5, 95) + 'vw'} - ${
       size / 2
     }px)`;
-    const image = document.createElement("img");
+    const image = document.createElement('img');
     image.src = balloonImageUrl;
     this.element.appendChild(image);
 
@@ -32,10 +32,10 @@ export default class Balloon {
     this.rise();
 
     // Add an event listener to the balloon
-    this.element.addEventListener("click", this.pop.bind(this));
+    this.element.addEventListener('click', this.pop.bind(this));
   }
 
-  rise() {
+  public rise() {
     this.element.style.animation = `rise ${this.duration}ms linear`;
 
     setTimeout(() => {
@@ -43,22 +43,22 @@ export default class Balloon {
     }, this.duration);
   }
 
-  remove() {
+  public remove() {
     this.element.remove();
   }
 
-  async pop() {
+  public async pop() {
     // Remove the balloon
     this.remove();
     // Play the pop sound
     this.popSound.play();
 
     // Get the stored value
-    const storedCount = (await storage.get("balloonCount")).balloonCount || 0;
+    const storedCount = (await storage.get('balloonCount')).balloonCount || 0;
     const balloonCount = storedCount + 1;
     // Store the new value
-    storage.set("balloonCount", { balloonCount });
+    storage.set('balloonCount', { balloonCount });
     // Send message with the new count
-    sendMessage({ action: "updateCounter", balloonCount });
+    sendMessage({ action: 'updateCounter', balloonCount });
   }
 }
