bf0d9bfc5d9a5dcdadbe25af9d991cc8fd8a94ba
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-09 16:59:28 +0100

    Add comments to background script

--- src/background/background.ts
+++ src/background/background.ts
@@ -21,31 +21,41 @@ const updateBadgeColors = () => {
 
 const setup = async () => {
   const balloonCount = (await storage.get('balloonCount'))?.balloonCount;
+  // Reset the counter if it's undefined
   if (balloonCount === undefined) {
     resetCounter();
   }
 
+  // Set badge number and colors
   setBadgeNumber(balloonCount || 0);
   updateBadgeColors();
 };
 
 let loopRunning = false;
 const loop = async () => {
+  // If the loop is already running, don't start another one
   if (loopRunning) return;
   loopRunning = true;
 
   while (true) {
+    // Wait between 0 and 10 minutes
     await sleep(generateRandomNumber(0, minutesToMilliseconds(10)));
+
+    // Get all active tabs
     chrome.tabs.query({ active: true }, (tabs) => {
+      // Select a random tab
       const num = Math.round(generateRandomNumber(0, tabs.length - 1));
       const tab = tabs[num];
       if (!tab.id) return;
       console.log(`Sending spawnBalloon to`, tab);
 
+      // Send the spawnBalloon message
       chrome.tabs.sendMessage(
         tab.id,
         { action: 'spawnBalloon' },
         (response) => {
+          // If there was an error, discard it
+          // Error is most likely 'Receiving end does not exist.' exception
           if (chrome.runtime.lastError) {
             chrome.runtime.lastError = undefined;
           }
