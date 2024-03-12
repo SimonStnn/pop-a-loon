f6f2ae55d558da2699f5dff75afe11c35b6b580f
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-12 21:55:16 +0100

    Add user schema

---
+++ api/schemas/user.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+export const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model('User', schema);
