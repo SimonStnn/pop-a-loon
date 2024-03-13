2e95682c330f867bd3a803245be6b43427a9b0fe
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-13 19:46:04 +0100

    Add user routes

---
+++ api/routes/user.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express';
+import User from '../schemas/user';
+
+const router = express.Router();
+
+router.get('/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findById(id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
+router.post('/new', async (req: Request, res: Response) => {
+  const { username, email, password } = req.query;
+
+  try {
+    const user = new User({ username, email, password });
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
+
+export default router;
