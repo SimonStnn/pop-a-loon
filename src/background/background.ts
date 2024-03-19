118bf1f6bb74930c1ad8e27be6e648d55d25381d
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-19 18:53:22 +0100

    Add remote configuration and user handling

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,7 +1,8 @@
 import { abbreviateNumber } from 'js-abbreviation-number';
-import { Message, initalConfig } from '@const';
+import { Message, User, initalConfig } from '@const';
 import { generateRandomNumber, minutesToMilliseconds, sleep } from '@utils';
 import storage from '@/storage';
+import remote from '@/remote';
 
 const resetCounter = () => {
   storage.set('balloonCount', { balloonCount: 0 });
@@ -31,8 +32,30 @@ const updateBadgeColors = () => {
       resetCounter();
     }
 
-    const config = await storage.get('config');
-    if (!config) await storage.set('config', initalConfig);
+    // Get the config from the remote
+    const remoteConfig = await remote.getConfiguration();
+
+    // Get the config from the local storage
+    let config = await storage.get('config');
+
+    // If the config is not in the local storage, use the default config
+    if (!config) {
+      config = initalConfig as any;
+    }
+    // Merge the remote config with the local config
+    await storage.set('config', { ...config, ...remoteConfig });
+
+    // Get the user from the local storage
+    let localUser = await storage.get('user');
+    // If the user is not in the local storage, get a new user from the remote
+    if (!localUser) {
+      const usr = await remote.getNewUser();
+      await storage.set('token', usr.token);
+      localUser = usr;
+    }
+    // Get the user from the remote and save it to the local storage
+    const user = await remote.getUser(localUser.id);
+    await storage.set('user', user);
 
     // Set badge number and colors
     setBadgeNumber(balloonCount || 0);
