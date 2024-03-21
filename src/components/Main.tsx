9c1f4c7a2775bfd442d5b5b55600d8a578c7bc3a
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-21 21:52:14 +0100

    Refactor Main component to include className prop

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { cn } from '@/utils';
+import { ClassValue } from 'clsx';
 
-export default (props: any) => {
+interface MainProps {
+  className?: ClassValue;
+  children?: ReactNode;
+}
+
+export default (props: MainProps) => {
   return (
-    <main className="m-auto my-2 flex w-4/5 flex-col gap-2">
+    <main
+      className={cn('m-auto my-2 flex w-4/5 flex-col gap-2', props.className)}
+    >
       {props.children}
     </main>
   );
