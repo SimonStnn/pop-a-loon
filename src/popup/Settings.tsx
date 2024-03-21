61af27ed075256b855df3f24feb60b0f878f2d89
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-21 21:54:05 +0100

    Add user data fetching and display in Settings component

--- src/popup/Settings.tsx
+++ src/popup/Settings.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import Header from '@/components/Header';
 import Main from '@/components/Main';
+import { User } from '@/const';
+import storage from '@/storage';
 
 export default () => {
+  const [user, setUser] = useState({} as User);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setUser(await storage.get('user'));
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header
@@ -16,7 +28,12 @@ export default () => {
       <Main>
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="username">Username</Label>
-          <Input type="email" id="username" placeholder="Username" />
+          <Input
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={user.username}
+          />
         </div>
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label>Reset Balloons</Label>
