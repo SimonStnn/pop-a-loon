c3a9f6ae5c56ffb7cb4b29898ad56ba140fa3aee
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-21 20:42:25 +0100

    Remove console.log statement in Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 export default (props: any) => {
-  console.log(typeof props);
-
   return (
     <main className="m-auto my-2 flex w-4/5 flex-col gap-2">
       {props.children}
