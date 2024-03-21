7f7d2539eb22b767b26dcda8b56068214e94b30d
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-21 21:54:21 +0100

    Update grid layout in Settings component

--- src/popup/Settings.tsx
+++ src/popup/Settings.tsx
@@ -25,7 +25,7 @@ export default () => {
         title="Settings"
         icons={[{ to: '/general', side: 'left', icon: faArrowLeft }]}
       />
-      <Main>
+      <Main className="grid gap-4">
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="username">Username</Label>
           <Input
