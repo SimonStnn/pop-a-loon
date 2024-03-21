dc72685e87c8e131a0ca909701e7d4afd1f27029
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-21 20:45:43 +0100

    Update popup width in index.tsx

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import General from './General';
 import Settings from './Settings';
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import stylesheet
 import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <div className="w-[256px]">
+  <div className="w-[480px]">
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
