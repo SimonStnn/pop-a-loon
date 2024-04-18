192566186ebd4c144913a604f6b525951101120a
SimonStnn <simon.stijnen.23@gmail.com>
2024-04-18 23:20:31 +0200

    Add spawn_chance constant to Default balloon class

--- src/balloons/default.ts
+++ src/balloons/default.ts
@@ -2,6 +2,8 @@ import Balloon from '@/balloon';
 import { generateRandomNumber } from '@/utils';
 
 export default class Default extends Balloon {
+  public static readonly spawn_chance = 0.95;
+
   getRandomDuration() {
     return generateRandomNumber(10000, 15000);
   }
