1f594fcd06ea9ed851d0567409bcfc233bb4b29e
SimonStnn <simon.stijnen.23@gmail.com>
2024-04-24 18:46:09 +0200

    Add environment variable checks in webpack.config.js close #113

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,22 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const { EnvironmentPlugin } = require('webpack');
 
+function checkEnvVar(varName, defaultValue) {
+  if (process.env[varName] === undefined) {
+    if (defaultValue !== undefined) {
+      process.env[varName] = defaultValue;
+    } else {
+      console.error(`${varName} environment variable not set`);
+      process.exit(1);
+    }
+  }
+}
+
+checkEnvVar('BROWSER');
+checkEnvVar('npm_package_version');
+checkEnvVar('npm_lifecycle_script');
+checkEnvVar('REMOTE', 'remote');
+
 let mode;
 if (process.env.npm_lifecycle_script.includes('development'))
   mode = 'development';
