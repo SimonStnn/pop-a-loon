bf07f37559e1b9783308116998f7f57dd539a866
SimonStnn <simon.stijnen.23@gmail.com>
2024-04-28 23:12:15 +0200

    Update Jest mocks for webextension-polyfill.ts

--- tests/__mocks__/webextension-polyfill.ts
+++ tests/__mocks__/webextension-polyfill.ts
@@ -1,8 +1,41 @@
+import { StorageStructure, initalConfig, storageKey } from '@const';
+
+const defaultStorage: StorageStructure = {
+  config: initalConfig,
+  token: '',
+  user: {
+    id: '',
+    username: '',
+    email: '',
+    count: 0,
+    updatedAt: '',
+    createdAt: '',
+  },
+};
+
+let mockStorage: StorageStructure = defaultStorage;
+
 export default {
   runtime: {
     getURL: jest.fn(),
     onInstalled: {
       addListener: jest.fn(),
+    },
+  },
+  storage: {
+    sync: {
+      get: jest.fn(async (key: storageKey) => {
+        return mockStorage[key];
+      }),
+      set: jest.fn(async (data) => {
+        mockStorage = { ...mockStorage, ...data };
+      }),
+      remove: jest.fn(async (key: storageKey) => {
+        delete mockStorage[key];
+      }),
+      clear: jest.fn(async () => {
+        mockStorage = defaultStorage;
+      }),
     },
   },
 };
