f5afd2a3a82a57863849a3be592a5da6a0c372ec
SimonStnn <simon.stijnen.23@gmail.com>
2024-04-30 22:12:45 +0200

    Update remote.test.ts with additional unit tests for remote module

--- tests/remote.test.ts
+++ tests/remote.test.ts
@@ -1,4 +1,4 @@
-import { mockFetchResponse } from './__mocks__/fetch';
+import { mockFetchResponse, user as mockedUser } from './__mocks__/fetch';
 import remote from '@/remote';
 
 describe('BackendAPI', () => {
@@ -9,6 +9,26 @@ describe('BackendAPI', () => {
   test('should return a response', async () => {
     const response = await remote.getStatus();
     expect(response).toBeDefined();
+    expect(fetchMock).toHaveBeenCalledTimes(1);
+  });
+
+  test('should be available', async () => {
+    const response = await remote.isAvailable();
+    expect(response).toBeTruthy();
+    expect(fetchMock).toHaveBeenCalledTimes(1);
+    const response2 = await remote.isAvailable();
+    expect(response2).toBeTruthy();
+    expect(fetchMock).toHaveBeenCalledTimes(1);
+    // Simulate time passing
+    remote['lastChecked'] = new Date(0);
+    const response3 = await remote.isAvailable();
+    expect(response3).toBeTruthy();
+    expect(fetchMock).toHaveBeenCalledTimes(2);
+  });
+
+  test('should return a user', async () => {
+    const response = await remote.getUser(mockedUser.id);
+    expect(response).toEqual(mockedUser);
     expect(fetchMock).toHaveBeenCalledTimes(1);
   });
 });
