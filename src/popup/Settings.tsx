99ba5443b1514b7539eae75543267994898d84da
SimonStnn <simon.stijnen.23@gmail.com>
2024-05-01 21:13:11 +0200

    Remove scrollbar from settings page

--- src/popup/Settings.tsx
+++ src/popup/Settings.tsx
@@ -6,7 +6,6 @@ import { z } from 'zod';
 import { Loader2, CheckIcon } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { ScrollArea } from '@/components/ui/scroll-area';
 import {
   Form,
   FormControl,
@@ -90,102 +89,98 @@ export default () => {
   return (
     <>
       <Main className="">
-        <ScrollArea>
-          <div className="h-[156px] grid gap-4 pl-2 pr-5 mr-[-8px]">
-            <section>
-              <Form {...form}>
-                <form
-                  className="grid gap-4"
-                  onSubmit={form.handleSubmit(onSubmit)}
-                >
-                  <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Username</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Username" {...field} />
-                        </FormControl>
-                        <FormDescription>
-                          This is your public display name.
-                        </FormDescription>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <Button type="submit" className="flex flex-row gap-1">
-                    Save
-                    {form.formState.isSubmitting && (
-                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                    )}
-                    {form.formState.isSubmitSuccessful && (
-                      <CheckIcon className="mr-2 h-4 w-4" />
-                    )}
-                  </Button>
-                </form>
-              </Form>
-            </section>
+        <div className="h-[156px] grid gap-4">
+          <section>
+            <Form {...form}>
+              <form
+                className="grid gap-4"
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Username" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="flex flex-row gap-1">
+                  Save
+                  {form.formState.isSubmitting && (
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  {form.formState.isSubmitSuccessful && (
+                    <CheckIcon className="mr-2 h-4 w-4" />
+                  )}
+                </Button>
+              </form>
+            </Form>
+          </section>
 
-            <section>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button
-                    variant="outline"
-                    className="border-destructive text-destructive hover:text-destructive py-1 px-2 h-7 w-full"
+          <section>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="border-destructive text-destructive hover:text-destructive py-1 px-2 h-7 w-full"
+                >
+                  Delete account
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Delete account</DialogTitle>
+                  <DialogDescription asChild>
+                    <Alert variant="destructive" className="text-destructive">
+                      <AlertCircle className="h-4 w-4" />
+                      <AlertTitle className="w-min">Danger</AlertTitle>
+                      <AlertDescription>
+                        This action is not reversible. Please be certain.
+                      </AlertDescription>
+                    </Alert>
+                  </DialogDescription>
+                </DialogHeader>
+                <Form {...deleteForm}>
+                  <form
+                    className="grid gap-4"
+                    onSubmit={deleteForm.handleSubmit(deleteFormOnSubmit)}
                   >
-                    Delete account
-                  </Button>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>Delete account</DialogTitle>
-                    <DialogDescription asChild>
-                      <Alert variant="destructive" className="text-destructive">
-                        <AlertCircle className="h-4 w-4" />
-                        <AlertTitle className="w-min">Danger</AlertTitle>
-                        <AlertDescription>
-                          This action is not reversible. Please be certain.
-                        </AlertDescription>
-                      </Alert>
-                    </DialogDescription>
-                  </DialogHeader>
-                  <Form {...deleteForm}>
-                    <form
-                      className="grid gap-4"
-                      onSubmit={deleteForm.handleSubmit(deleteFormOnSubmit)}
-                    >
-                      <FormField
-                        control={deleteForm.control}
-                        name="username"
-                        render={({ field }) => (
-                          <>
-                            <FormItem>
-                              <FormLabel>
-                                Enter your username <b>{user?.username}</b> to
-                                continue:
-                              </FormLabel>
-                              <FormControl>
-                                <div className="flex w-full max-w-sm items-center space-x-2">
-                                  <Input id="usernameDelete" {...field} />
-                                  <Button type="submit">
-                                    Delete my account
-                                  </Button>
-                                </div>
-                              </FormControl>
-                              <FormDescription></FormDescription>
-                              <FormMessage />
-                            </FormItem>
-                          </>
-                        )}
-                      />
-                    </form>
-                  </Form>
-                </DialogContent>
-              </Dialog>
-            </section>
-          </div>
-        </ScrollArea>
+                    <FormField
+                      control={deleteForm.control}
+                      name="username"
+                      render={({ field }) => (
+                        <>
+                          <FormItem>
+                            <FormLabel>
+                              Enter your username <b>{user?.username}</b> to
+                              continue:
+                            </FormLabel>
+                            <FormControl>
+                              <div className="flex w-full max-w-sm items-center space-x-2">
+                                <Input id="usernameDelete" {...field} />
+                                <Button type="submit">Delete my account</Button>
+                              </div>
+                            </FormControl>
+                            <FormDescription></FormDescription>
+                            <FormMessage />
+                          </FormItem>
+                        </>
+                      )}
+                    />
+                  </form>
+                </Form>
+              </DialogContent>
+            </Dialog>
+          </section>
+        </div>
       </Main>
     </>
   );
