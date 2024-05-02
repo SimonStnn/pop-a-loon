353081d3a03c7fca9e45b4caba2a9e5c99466ff0
SimonStnn <simon.stijnen.23@gmail.com>
2024-05-02 18:55:18 +0200

    Refactor General.tsx to use @radix-ui/react-tabs for tab navigation

--- src/popup/General.tsx
+++ src/popup/General.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import Main from '@/components/Main';
 import Leaderboard from '@/components/Leaderboard';
 
@@ -6,8 +7,20 @@ export default () => {
   return (
     <>
       <Main>
-        <h2 className="text-lg">Leaderboard</h2>
-        <Leaderboard />
+        <Tabs defaultValue="all-time">
+          <TabsList className="grid grid-cols-3 w-full">
+            <TabsTrigger value="all-time">All time</TabsTrigger>
+            <TabsTrigger value="daily">Daily</TabsTrigger>
+            <TabsTrigger value="stats">Your stats</TabsTrigger>
+          </TabsList>
+          <TabsContent value="all-time">
+            <Leaderboard />
+          </TabsContent>
+          <TabsContent value="daily">
+            <Leaderboard />
+          </TabsContent>
+          <TabsContent value="stats">Your stats here.</TabsContent>
+        </Tabs>
       </Main>
     </>
   );
