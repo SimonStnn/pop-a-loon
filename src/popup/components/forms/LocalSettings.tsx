810c8db14115776403c9ee58ce08790519011616
SimonStnn <simon.stijnen.23@gmail.com>
2024-05-14 18:20:06 +0200

    Add volume slider to LocalSettings #67

--- src/popup/components/forms/LocalSettings.tsx
+++ src/popup/components/forms/LocalSettings.tsx
@@ -10,11 +10,16 @@ import {
   FormLabel,
   FormMessage,
 } from '@components/ui/form';
-import { Input } from '@components/ui/input';
+import { Slider } from '@components/ui/slider';
+import { Default as DefaultBalloon } from '@/balloons';
 import storage from '@/storage';
 
+const MIN_POP_VOLUME = 0;
+const VOLUME_STEP = 20;
+const MAX_POP_VOLUME = 100;
+
 const formSchema = z.object({
-  popVolume: z.number().int().min(0).max(100),
+  popVolume: z.number().int().min(MIN_POP_VOLUME).max(MAX_POP_VOLUME),
 });
 
 export default () => {
@@ -24,6 +29,26 @@ export default () => {
       popVolume: popVolume,
     },
   });
+  const popSound = new DefaultBalloon().popSound;
+
+  const onPopVolumeChange = async (popVolume: number) => {
+    // Save volume to storage
+    const config = await storage.get('config');
+    await storage.set('config', {
+      ...config,
+      popVolume,
+    });
+
+    setPopVolume(popVolume);
+    console.log(
+      'Pop volume changed to',
+      (await storage.get('config')).popVolume
+    );
+
+    // Play the pop sound
+    popSound.volume = popVolume / 100;
+    popSound.play();
+  };
 
   useEffect(() => {
     const loadVolume = async () => {
@@ -40,11 +65,20 @@ export default () => {
         <FormField
           control={form.control}
           name="popVolume"
-          render={({ field }) => (
+          render={({ field: { onChange } }) => (
             <FormItem>
               <FormLabel>Pop Volume</FormLabel>
               <FormControl>
-                <Input {...field} />
+                <Slider
+                  min={MIN_POP_VOLUME}
+                  max={MAX_POP_VOLUME}
+                  step={VOLUME_STEP}
+                  defaultValue={[popVolume]}
+                  onValueChange={(vals) => {
+                    onChange(vals[0]);
+                    onPopVolumeChange(vals[0]);
+                  }}
+                />
               </FormControl>
               <FormDescription>
                 The volume of the pop sound when a balloon is popped.
