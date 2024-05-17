18ae801ceddd99f0db7b7a5b65601aa463b099a9
SimonStnn <simon.stijnen.23@gmail.com>
2024-05-17 21:50:10 +0200

    Remove getRandomDuration method from Default balloon class

--- src/balloons/default.ts
+++ src/balloons/default.ts
@@ -4,8 +4,4 @@ import { generateRandomNumber } from '@/utils';
 export default class Default extends Balloon {
   public readonly name = 'default';
   public static readonly spawn_chance = 0.95;
-
-  getRandomDuration() {
-    return generateRandomNumber(10000, 15000);
-  }
 }
