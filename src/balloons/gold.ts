e38822cd754bd76a4dabf0b5d064d614ba22f600
SimonStnn <simon.stijnen.23@gmail.com>
2024-06-29 23:00:40 +0200

    Make the golden balloon rise slower

--- src/balloons/gold.ts
+++ src/balloons/gold.ts
@@ -5,6 +5,8 @@ export default class Confetti extends Default {
   // @ts-ignore
   public readonly name = 'gold';
 
+  public readonly riseDurationThreshold: [number, number] = [15000, 20000];
+
   constructor() {
     super();
     this.options.imageUrl = `/../${this.name}/balloon.svg`;
