c28c44e0099c12ddb79ba6faa122484adc90561b
SimonStnn <simon.stijnen.23@gmail.com>
2024-07-01 18:01:46 +0200

    Make gold balloon swing slower

--- src/balloons/gold.ts
+++ src/balloons/gold.ts
@@ -1,7 +1,7 @@
 import Default from './default';
 
 export default class Gold extends Default {
-  public static readonly spawn_chance: number = 0.05;
+  public static readonly spawn_chance: number = 1.05;
   // @ts-ignore
   public readonly name = 'gold';
 
@@ -10,5 +10,7 @@ export default class Gold extends Default {
     this.options.imageUrl = `/../${this.name}/balloon.svg`;
     this.riseDurationThreshold[0] = 15000;
     this.riseDurationThreshold[1] = 20000;
+    this.swingDurationThreshold[0] = 3;
+    this.swingDurationThreshold[1] = 4;
   }
 }
