e98efd37d3393c88259e2f646e8a78772964636a
SimonStnn <simon.stijnen.23@gmail.com>
2024-07-02 21:24:54 +0200

    Update confetti and gold options and name

--- src/balloons/gold.ts
+++ src/balloons/gold.ts
@@ -1,16 +1,17 @@
-import Default from './default';
+import Default, { BalloonOptions } from './default';
 
 export default class Gold extends Default {
   public static readonly spawn_chance: number = 0.05;
   // @ts-ignore
-  public readonly name = 'gold';
+  public get name(): 'gold' {
+    return 'gold';
+  }
 
-  constructor() {
-    super();
-    this.options.imageUrl = `/../${this.name}/balloon.svg`;
-    this.riseDurationThreshold[0] = 15000;
-    this.riseDurationThreshold[1] = 20000;
-    this.swingDurationThreshold[0] = 3;
-    this.swingDurationThreshold[1] = 4;
+  public get options(): BalloonOptions {
+    return {
+      ...super.options,
+      riseDurationThreshold: [15000, 20000],
+      swingDurationThreshold: [3, 4],
+    };
   }
 }
