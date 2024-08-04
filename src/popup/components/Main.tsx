40978a4720a2ab3579160863092b74d4d728e0f4
SimonStnn <simon.stijnen.23@gmail.com>
2024-08-04 17:54:59 +0200

    Give Main component a min-h so the navigation fits the popup

--- src/popup/components/Main.tsx
+++ src/popup/components/Main.tsx
@@ -1,6 +1,5 @@
-import React, { ReactNode } from 'react';
 import { ClassValue } from 'clsx';
-
+import React, { ReactNode } from 'react';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { cn } from '@/utils';
 
@@ -13,7 +12,10 @@ export default (props: MainProps) => {
   return (
     <ScrollArea>
       <main
-        className={cn('m-auto my-2 flex w-4/5 flex-col gap-2', props.className)}
+        className={cn(
+          'm-auto my-2 flex w-4/5 flex-col gap-2 min-h-[120px]',
+          props.className
+        )}
       >
         {props.children}
       </main>
