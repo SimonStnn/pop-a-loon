e63162317b2d8cb552e7368248ea24299b82cd14
SimonStnn <simon.stijnen.23@gmail.com>
2024-08-18 18:26:57 +0200

    Modify calendar to be more compact #315

--- src/popup/components/ui/calendar.tsx
+++ src/popup/components/ui/calendar.tsx
@@ -17,8 +17,8 @@ function Calendar({
       showOutsideDays={showOutsideDays}
       className={cn('p-3', className)}
       classNames={{
-        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
-        month: 'space-y-4',
+        months: 'flex flex-col sm:flex-row space-y-1 sm:space-x-4 sm:space-y-0',
+        month: 'space-y-1',
         caption: 'flex justify-center pt-1 relative items-center',
         caption_label: 'text-sm font-medium',
         nav: 'space-x-1 flex items-center',
@@ -31,12 +31,12 @@ function Calendar({
         table: 'w-full border-collapse space-y-1',
         head_row: 'flex',
         head_cell:
-          'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
-        row: 'flex w-full mt-2',
-        cell: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
+          'text-muted-foreground rounded-md w-7 font-normal text-[0.8rem]',
+        row: 'flex w-full mt-0.5',
+        cell: 'h-7 w-7 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
         day: cn(
           buttonVariants({ variant: 'ghost' }),
-          'h-9 w-9 p-0 font-normal aria-selected:opacity-100'
+          'h-7 w-7 p-0 font-normal aria-selected:opacity-100'
         ),
         day_range_end: 'day-range-end',
         day_selected:
