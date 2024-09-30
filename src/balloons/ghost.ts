7577e295412655f4c494e26831be3fb86f5e60b8
joe boone <joejoboone@hotmail.com>
2024-09-30 23:36:21 +0200

    Added Ghost balloon

---
+++ src/balloons/ghost.ts
@@ -0,0 +1,18 @@
+import Default, { BalloonOptions } from './default';
+
+export default class Ghost extends Default {
+  public static readonly spawn_chance: number = 0.1;
+  // @ts-ignore
+  public get name(): 'ghost' {
+    return 'ghost';
+  }
+
+  public get options(): BalloonOptions {
+    return {
+      ...super.options,
+      imageUrl: 'balloon.svg',
+      riseDuration: [15000, 20000],
+      swingDuration: [3, 4],
+    };
+  }
+}
