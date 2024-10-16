6234d156697a70aec60b4b58d9cd8267699c56f6
BanaantjeJowie <boonej394@gmail.com>
2024-10-16 16:24:47 +0200

    Move ghost styles to separate stylesheet #340
    Co-authored-by: SimonStnn simon.stijnen.23@gmail.com

--- src/balloons/ghost.ts
+++ src/balloons/ghost.ts
@@ -1,4 +1,9 @@
 import Default, { BalloonOptions } from './default';
+import { random } from '@/utils';
+
+type GhostOptions = BalloonOptions & {
+  fadeSpeed: number | [number, number];
+};
 
 export default class Ghost extends Default {
   public static readonly spawn_chance: number = 0.1;
@@ -7,13 +12,25 @@ export default class Ghost extends Default {
     return 'ghost';
   }
 
-  public get options(): BalloonOptions {
+  public get options(): GhostOptions {
     return {
       ...super.options,
       imageUrl: 'balloon.svg',
       riseDuration: [15000, 20000],
       swingDuration: [3, 4],
       swingOffset: 25,
+      fadeSpeed: [2000, 3000],
     };
+  }
+
+  public build(): void {
+    super.build();
+    this.importStylesheet('ghost.css');
+
+    const animationSpeed =
+      typeof this.options.fadeSpeed === 'number'
+        ? this.options.fadeSpeed
+        : random(this.options.fadeSpeed[0], this.options.fadeSpeed[1]);
+    this.element.style.setProperty('--animation-speed', `${animationSpeed}ms`);
   }
 }
