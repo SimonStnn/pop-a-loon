f24dfd38af73b99eb32747e74f0d5c5133d3dfdf
SimonStnn <simon.stijnen.23@gmail.com>
2024-11-01 17:41:22 +0100

    Add a toggle to toggle global stats in history graph

--- src/popup/pages/Statistics.tsx
+++ src/popup/pages/Statistics.tsx
@@ -1,5 +1,5 @@
 import { format, subDays } from 'date-fns';
-import { Calendar as CalendarIcon } from 'lucide-react';
+import { Calendar as CalendarIcon, Globe } from 'lucide-react';
 import React from 'react';
 import { DateRange } from 'react-day-picker';
 import Main from '@/components/Main';
@@ -19,6 +19,7 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { Skeleton } from '@/components/ui/skeleton';
+import { Toggle } from '@/components/ui/toggle';
 import storage from '@/managers/storage';
 import { cn } from '@/utils';
 
@@ -27,6 +28,7 @@ export default () => {
     from: subDays(new Date(), 7),
     to: new Date(),
   });
+  const [global, setGlobal] = React.useState(true);
 
   const handleDatePresetChange = async (value: string) => {
     const minDate = new Date('2024-07-01');
@@ -67,61 +69,70 @@ export default () => {
 
   return (
     <Main>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={'outline'}
-            className={cn(
-              'mb-8 justify-start text-left font-normal',
-              !date && 'text-muted-foreground'
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, 'LLL dd, y')} -{' '}
-                  {format(date.to, 'LLL dd, y')}
-                </>
+      <div className="mb-8 flex flex-row gap-1">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              id="date"
+              variant={'outline'}
+              className={cn(
+                'flex-grow justify-start text-left font-normal',
+                !date && 'text-muted-foreground'
+              )}
+            >
+              <CalendarIcon className="mr-2 size-4" />
+              {date?.from ? (
+                date.to ? (
+                  <>
+                    {format(date.from, 'LLL dd, y')} -{' '}
+                    {format(date.to, 'LLL dd, y')}
+                  </>
+                ) : (
+                  format(date.from, 'LLL dd, y')
+                )
               ) : (
-                format(date.from, 'LLL dd, y')
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent
-          className="flex w-full flex-col gap-2 p-2"
-          align="start"
+                <span>Pick a date</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            className="flex w-full flex-col gap-2 p-2"
+            align="start"
+          >
+            <Select onValueChange={handleDatePresetChange}>
+              <SelectTrigger>
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="last-week">Last week</SelectItem>
+                <SelectItem value="last-month">Last month</SelectItem>
+                <SelectItem value="last-3-months">Last 3 months</SelectItem>
+                <SelectItem value="all-time">All time</SelectItem>
+              </SelectContent>
+            </Select>
+            <Calendar
+              initialFocus
+              className="p-0"
+              mode="range"
+              defaultMonth={date?.from}
+              selected={date}
+              onSelect={setDate}
+              numberOfMonths={1}
+              fromDate={new Date('2024-07-01')}
+              toDate={new Date()}
+            />
+          </PopoverContent>
+        </Popover>
+        <Toggle
+          variant="outline"
+          aria-label="Toggle global stats"
+          onClick={(e) => setGlobal(!global)}
         >
-          <Select onValueChange={handleDatePresetChange}>
-            <SelectTrigger>
-              <SelectValue placeholder="Select" />
-            </SelectTrigger>
-            <SelectContent position="popper">
-              <SelectItem value="last-week">Last week</SelectItem>
-              <SelectItem value="last-month">Last month</SelectItem>
-              <SelectItem value="last-3-months">Last 3 months</SelectItem>
-              <SelectItem value="all-time">All time</SelectItem>
-            </SelectContent>
-          </Select>
-          <Calendar
-            initialFocus
-            className="p-0"
-            mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={1}
-            fromDate={new Date('2024-07-01')}
-            toDate={new Date()}
-          />
-        </PopoverContent>
-      </Popover>
+          <Globe className="size-4" />
+        </Toggle>
+      </div>
       {date?.from && date?.to ? (
-        <Graph startDate={date?.from} endDate={date?.to} />
+        <Graph startDate={date?.from} endDate={date?.to} global={global} />
       ) : (
         <Skeleton className="h-[200px] w-full" />
       )}
